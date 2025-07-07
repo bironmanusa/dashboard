206a861918e19853a7f6b94c1307c827f9be3859
Mateo Mayorga <103150863+bironmanusa@users.noreply.github.com>
2025-07-07 19:29:47 +0000

    Hooks - (useState y useEffect)

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,12 @@
 import  SelectorUI  from './components/SelectorUI.tsx'
+import IndicatorUI from './components/IndicatorUI.tsx'
+import DataFetcher from './functions/DataFetcher';
 import './App.css'
 import { Grid } from '@mui/material'
 
 function App() {
+
+  const dataFetcherOutput = DataFetcher();
 
   return (
     <>
@@ -10,31 +14,67 @@ function App() {
         <h1>Bienvenido al Dashboard</h1>
       </div>
       <Grid container spacing={5} justifyContent="center" alignItems="Center">
+        {/* Encabezado */}
+        <Grid size={{ xs: 12 }}>Elemento: Encabezado</Grid>
 
-         {/* Encabezado */}
-         <Grid size={{ xs: 12 }}>Elemento: Encabezado</Grid>
+        {/* Alertas */}
+        <Grid size={{ xs: 12 }}>Elemento: Alertas</Grid>
 
-         {/* Alertas */}
-         <Grid size={{ xs: 12 }}>Elemento: Alertas</Grid>
+        {/* Selector */}
+        <Grid size={{ xs: 12, md: 3}}><SelectorUI /></Grid>
 
-         {/* Selector */}
-         <Grid size={{ xs: 12, md: 3}}><SelectorUI /></Grid>
+        {/* Indicadores */}
+        <Grid container size={{ xs: 12, md: 9 }} >
 
-         {/* Indicadores */}
-         <Grid size={{ xs: 12, md: 9}}>Elemento: Indicadores</Grid>
+          {/* Renderizado condicional de los datos obtenidos */}
+          {dataFetcherOutput.loading && <p>Cargando datos...</p>}
+          {dataFetcherOutput.error && <p>Error: {dataFetcherOutput.error}</p>}
+          {dataFetcherOutput.data && (
+          <>
 
-         {/* Gráfico */}
-         <Grid
+          {/* Indicadores con datos obtenidos */}
+
+          <Grid size={{ xs: 12, md: 3 }} >
+            <IndicatorUI
+              title='Temperatura (2m)'
+              description={dataFetcherOutput.data.current.temperature_2m + " " + dataFetcherOutput.data.current_units.temperature_2m} />
+          </Grid>
+
+          <Grid size={{ xs: 12, md: 3 }}>
+            <IndicatorUI
+              title='Temperatura aparente'
+              description={dataFetcherOutput.data.current.apparent_temperature + " " + dataFetcherOutput.data.current_units.apparent_temperature} />
+            </Grid>
+
+            <Grid size={{ xs: 12, md: 3 }}>
+              <IndicatorUI
+                title='Velocidad del viento'
+                description={dataFetcherOutput.data.current.wind_speed_10m + " " + dataFetcherOutput.data.current_units.wind_speed_10m} />
+            </Grid>
+
+            <Grid size={{ xs: 12, md: 3 }}>
+              <IndicatorUI
+                title='Humedad relativa'
+                description={dataFetcherOutput.data.current.relative_humidity_2m + " " + dataFetcherOutput.data.current_units.relative_humidity_2m} />
+            </Grid>
+
+            </>
+            )}
+        </Grid>
+
+
+        {/* Gráfico */}
+        <Grid
           sx={{ dispaly: { xs: "none", md: "block" } }} 
           size={{ xs: 12, md: 6}}>
             Elemento: Gráfico
-         </Grid>
+          </Grid>
 
-         {/* Tabla */}
-         <Grid size={{ xs: 12, md: 6}}>Elemento: Tabla</Grid>
+        {/* Tabla */}
+        <Grid size={{ xs: 12, md: 6}}>Elemento: Tabla</Grid>
 
-         {/* Información adicional */}
-         <Grid size={{ xs: 12, md: 12}}>Elemento: Información adicional</Grid>
+        {/* Información adicional */}
+        <Grid size={{ xs: 12, md: 12}}>Elemento: Información adicional</Grid>
 
       </Grid>
     </>
---
+++ src/functions/DataFetcher.tsx
@@ -0,0 +1,41 @@
+import { useEffect, useState } from "react";
+import type { OpenMeteoResponse } from "../types/DashboardTypes";
+
+interface DataFetcherOutPut{
+    data: OpenMeteoResponse | null;
+    loading: boolean;
+    error: string | null;
+}
+
+export default function DataFetcher(): DataFetcherOutPut {
+    const [data, setData] = useState<OpenMeteoResponse | null>(null);
+    const [loading, setLoading] = useState<boolean>(true);
+    const [error, setError] = useState<string | null>(null);
+
+    useEffect(() => {
+        const url = "https://api.open-meteo.com/v1/forecast?latitude=52.52&longitude=13.41&hourly=temperature_2m&current=temperature_2m,relative_humidity_2m,apparent_temperature,wind_speed_10m&timezone=America%2FChicago";
+
+        const fetchData = async () => {
+            try{
+                const response = await fetch(url);
+                if(!response.ok) {
+                    throw new Error(`HTTP error! status: ${response.status} - ${response.statusText}`);
+                }
+
+                const result: OpenMeteoResponse = await response.json();
+                setData(result);
+            }catch (err: any){
+                if(err instanceof Error) {
+                    setError(err.message);
+                }else{
+                    setError("Ocurrió un error desconocido al obtener los datos.");
+                }
+            }finally{
+                setLoading(false);
+            }
+        };
+        fetchData();
+    }, [])
+
+    return { data, loading, error };
+}
